b55fd01a152a2d4ebb767be41c3df5c26f05963f
Robert A Petit III <robbie.petit@gmail.com>
2020-11-30 18:05:57 -0500

    add quast report parsing

--- bactopia/parsers/assembly.py
+++ bactopia/parsers/assembly.py
@@ -1,10 +1,10 @@
 from .generic import get_file_type, parse_json, parse_table
-ACCEPTED_FILES = [".fna.json", "checkm-results.txt"]
+ACCEPTED_FILES = [".fna.json", "checkm-results.txt", "transposed_report.tsv"]
 
 
 def parse(filename):
     filetype = get_file_type(ACCEPTED_FILES, filename)
     if filetype == ".fna.json":
         return parse_json(filename)
-    elif filetype == "checkm-results.txt":
+    elif filetype == "checkm-results.txt" or filetype == "transposed_report.tsv":
         return parse_table(filename)[0]
