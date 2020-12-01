ca8e1b595d4f518e300a7a1fc502c9d75ee92ce6
Robert A Petit III <robbie.petit@gmail.com>
2020-11-30 20:22:19 -0500

    remove typo

--- bactopia/parsers/mlst.py
+++ bactopia/parsers/mlst.py
@@ -2,17 +2,6 @@ from .generic import get_file_type, parse_json, parse_table
 ACCEPTED_FILES = ["blast.json", "mlst_report.tsv"]
 
 
-def _parse_annotation(filename):
-    """Parse Prokka summary text file."""
-    results = {}
-    with open(filename, 'rt') as fh:
-        for line in fh:
-            line = line.rstrip()
-            key, val = line.split(":")
-            results[key] = val.lstrip()
-    return results
-
-
 def parse(filename):
     filetype = get_file_type(ACCEPTED_FILES, filename)
     if filetype == "blast.json":
