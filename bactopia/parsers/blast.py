b0800e813cb014e651d72a0399ae852cc0e538bb
Robert A Petit III <robbie.petit@gmail.com>
2020-11-30 23:25:49 -0500

    add parser (with cleanup) for blast results

---
+++ bactopia/parsers/blast.py
@@ -0,0 +1,58 @@
+from .generic import get_file_type, parse_json
+ACCEPTED_FILES = [".json", 'plsdb.txt']
+
+
+def parse(filename):
+    filetype = get_file_type(ACCEPTED_FILES, filename)
+    if filetype == ".json":
+        return _parse_blast(parse_json(filename))
+    elif filetype == "plsdb.txt":
+        return _parse_plsdb(filename)
+
+
+def _parse_blast(jsondata):
+    """Parse BLAST JSON output and clean it up."""
+    results = {"program": None, 'queries': {}}
+    for row in jsondata['BlastOutput2']:
+        report = row['report']
+        if not results["program"]:
+            results['program'] = report['program']
+            results['version'] = report['version']
+            results['db'] = report['search_target']['db']
+            results['params'] = report['params']
+
+        search = report['results']['search']
+        query_title = search['query_title']
+        if query_title not in results['queries']:
+            results['queries'][query_title] = {'query_len': search['query_len'], 'hits': [], 'message': ''}
+
+        if search['hits']:
+            for hit in search['hits']:
+                results['queries'][query_title]['hits'].append({
+                    'subject_title': hit['description'][0]['title'].split()[0],
+                    'subject_len': hit['len'],
+                    'hsps': hit['hsps']
+                })
+        
+        if 'message' in search:
+            results['queries'][query_title]['message'] = search['message']
+    return results
+
+
+def _parse_plsdb(filename):
+    """Cleanup PLSDB JSON, then use _parse_blast."""
+    import json
+    merged_json = None
+    with open(filename, 'rt') as fh:
+        entry = []
+        entries = []
+        for line in fh:
+            entry.append(line)
+            if line.startswith("}"):
+                jsondata = json.loads(''.join(entry))
+                if merged_json:
+                    merged_json['BlastOutput2'].extend(jsondata['BlastOutput2'])
+                else:
+                    merged_json = jsondata
+                entry.clear()
+    return _parse_blast(merged_json)
